7d163844e623c6c3411b88e04b8aad6fe63d94eb
Attiano <attiano@ucla.edu>
2020-05-09 23:48:10 -0700

    tested and benchmarked grovers algorithm, figures generated in the same way as pratik

--- attiano/grover.py
+++ attiano/grover.py
@@ -8,12 +8,38 @@ from itertools import combinations
 import numpy as np
 import math
 from operator import add
+import time
+import matplotlib.pyplot as plt
 
-num_trials = 10
+def convert_n_bit_string_to_int(x,n):
+	"""
+	inputs: x - array of ints that represents an unsigned bit string
+			n is the number of bits
+	
+	outputs: integer corresponding to x's unsigned value
+	"""
+		integ = 0
+		for idx, val in enumerate(x):
+			integ += val*2^(n-idx-1)
+		return int(integ)
 
 
+def check_correctness(func,result):
+	"""
+	inputs: func - np array of ints that represnts a function mapping with the index as input
+			result - np array of ints that represents an unsigned bit string
+	
+	outputs: bool if the index that corresponds to result is 1, then f(result) = 1
+	"""
+	x = convert_n_bit_string_to_int(result)
+	return (func[x] == 1)
 
 def create_minus_gate(n):
+	"""
+	inputs: n (int) - number of qubits
+	
+	outputs: minus (np array of ints) - NxN matrix with -1 on the diagonal
+	"""
 	N = 2**n
 	
 	minus = np.identity(N)
@@ -71,10 +97,20 @@ def all_f(n,a):
 	return func_list
 
 def calc_lim(a,n):
+	"""
+	inputs: int n = number of qubits, int a = number of items that return 1 for function
+	
+	outputs: int k = number of iterations to run grovers algorithm
+	"""
 	N = 2**n
 	#theta = np.arcsin(a/N)
-	theta = 1/np.sqrt(N)
-	k = math.floor(abs(((np.pi)/(4*theta)) - 0.5))
+	theta = a/np.sqrt(N)
+	#heuristically determined if n<4 you overshoot if you round, and if n>4 you undershoot if you floor
+	#didnt test past n=6 as each trial took non-negligible time to compile and run
+	if n < 4:
+		k = math.floor(abs(((np.pi)/(4*theta)) - 0.5))
+	else:
+		k = math.round(abs(((np.pi)/(4*theta)) - 0.5))
 	return int(k)
 
 def grover(z0,zf,n,a):
@@ -92,14 +128,15 @@ def grover(z0,zf,n,a):
 	p += zf
 	p += z0
 	p += minus_def
+	ro = p.declare('ro', 'BIT', n)
 
 	#1. Hadamard each qubit
 	for i in range(n):
 		p += H(i)
-	
+
 	#setup inputs to multi-bit gates
 	num_arr = list(range(n))
-	
+
 	#2. apply G = -(H^n)zo(H^n)zf k times
 	k = calc_lim(a,n)
 
@@ -115,37 +152,178 @@ def grover(z0,zf,n,a):
 		for i in range(n):
 			p += H(i)
 			p += minus_gate(i)
-	structure = "%dq-qvm" % (n)
+		#measure each qubit
+		for qubit in range(n):
+			p += MEASURE(qubit, ro[qubit])
+			
+	#setup quantum computer
+	structure = "%dq-qvm" %(n)
 	qc = get_qc(structure)
-	#qvm.compiler.client.timeout = 1000000
-
-
-	result = qc.run_and_measure(p, trials=num_trials)
-	#measure all qubits and return them as a
-	a = []
-	for value in result.values():
-		a.append(value[0])
-
-	return a
-		
+	qc.compiler.client.timeout = 60000
 	
+	print("Starting compilation")
+	start = time.time()
+	executable = qc.compile(p)
+	end = time.time()
+	compile_time = int((end - start) * 1000)
+	print("Compilation finished, time: %d ms"%compile_time)
+	
+	print("Starting quantum computer")
+	start = time.time()
+	results = qc.run(executable)
+	end = time.time()
+	run_time = int((end - start) * 1000)
+	print("Quantum computer finished, Run-time: %d ms"%run_time)
+	
+	y = [ measurement for measurement in results[0] ]
+
+
+
+	return y, compile_time, run_time
+
+#test code	
+class test_harness(unittest.TestCase):
+	
+	def test_correct(self):
+		for a in range(1,3): 
+			for n in range(1,5):
+				all = all_f(n,a)
+				result = True
+				for func in all:
+					z0 = create_z0(n)
+					zf = create_zf(func,n)
+					z_0 = DefGate("Z0", z0)
+					z_f = DefGate("ZF", zf)
+					result_string, compile_time, run_time = grover(z_0,z_f,n,a)
+					result =  result and check_correctness(func,result_string)
+				self.assertTrue( result )
+		
+
+
 if __name__== '__main__':
 	num_inputs = len(sys.argv)
 	inputs = sys.argv 
-	#assume f and all inputs are given on CLI, even though this is unrealistic and poor practice
-	print("inputs: ")
-	print(sys.argv)
-	print("output: ")
-	n = 2 
-	a = 1
-	all = all_f(n,a)
-	for func in all:
-		print(func)
-		z0 = create_z0(n)
-		zf = create_zf(func,n)
-		z_0 = DefGate("Z0", z0)
-		z_f = DefGate("ZF", zf)
-		result = grover(z_0,z_f,n,a)
-		print(result)
+	
+	
 
-		
+	#benchmarking code
+	time_out_val = 10000
+	#set qubit range
+	n_min = 1
+	n_max = 6
+	n_list = list(range(n_min,n_max+1))
+	#num_times increases the number of trials time averaged over
+	#set equal to 1 as I average over all the possible functions for a given n
+	num_times = 1
+	compile_times = np.zeros([(n_max-n_min)+1, num_times])
+	compute_times = np.zeros([(n_max-n_min)+1, num_times])
+
+	#compute average compile and compute time for all functions for a range of n values and a given 'a'
+	for ind, n in enumerate(n_list):
+		for iter_ind in range(num_times):
+			a = 1
+			all = all_f(n,a)
+			avg_compute = 0
+			avg_compile = 0
+			for func in all:
+				print(func)
+				z0 = create_z0(n)
+				zf = create_zf(func,n)
+				z_0 = DefGate("Z0", z0)
+				z_f = DefGate("ZF", zf)
+				result, compile_time, run_time = grover(z_0,z_f,n,a)
+				avg_compile += compile_time
+				avg_compute += run_time
+				print(result)
+				#check_correctness(func,result)
+			compute_times[ind, iter_ind] = avg_compute/len(all)
+			compile_times[ind, iter_ind] = avg_compile/len(all)
+			
+	#testing for uf computation time at a fixed n
+	input_list = list(range(64)) #64 is (4 qubits choose 2) -> 2^4 choose 2 -> number of possible functions with a=2
+	uf_compute_times = np.zeros([64,num_times])
+	for iter_ind in range(num_times):
+		a = 1
+		n = 4
+		all = all_f(n,a)
+		for ind,func in enumerate(all):
+			print(func)
+			z0 = create_z0(n)
+			zf = create_zf(func,n)
+			z_0 = DefGate("Z0", z0)
+			z_f = DefGate("ZF", zf)
+			result, compile_time, run_time = grover(z_0,z_f,n,a)
+			print(result)
+			uf_compute_times[ind, iter_ind] = run_time
+	
+	#%% Save the data
+
+	np.savez('grover_benchmarking.npz', n_list = n_list,compile_times = compile_times,compute_times = compute_times)
+	np.savez('grover_uf.npz', input_list = input_list,uf_compute_times=uf_compute_times)
+	   
+	#%% Load data
+
+	data = np.load('grover_benchmarking.npz')
+	n_list = data['n_list']
+	compilation_times = data['compile_times']
+	computation_times = data['compute_times']
+	uf_data = np.load('grover_uf.npz')
+	input_list = uf_data['input_list']
+	uf_compute_times = uf_data['uf_compute_times']
+
+	#%%
+	avg_time_compile = np.sum(compilation_times,1)/np.shape(compilation_times)[1]
+	avg_time_compute = np.sum(computation_times,1)/np.shape(computation_times)[1]
+
+	#%% Plot and save 
+
+	#plot compilation time vs n
+	plt.rcParams["font.family"] = "serif"
+	fig = plt.figure(figsize=(16,10))
+
+	z = np.polyfit(n_list, avg_time_compile, 10)
+	p = np.poly1d(z)
+
+	plt.plot(np.linspace(n_list[0], n_list[-1] + 0.1, 100), p(np.linspace(n_list[0], n_list[-1] + 0.1, 100)), ls = '-', color = 'r')
+	plt.plot(n_list, avg_time_compile, ls = '', markersize = 15, marker = '.',label = 'M') #, ls = '--'
+	plt.title('Compilation time scaling for Grovers algorithm, a = 1', fontsize=25)
+	plt.xlabel('n (bit string length)',fontsize=20)
+	plt.ylabel('Average time of compilation (ms)',fontsize=20)
+	plt.xticks(fontsize=15)
+	plt.yticks(fontsize=15)
+
+
+	fig.savefig('Figures/grover_compile_a1.png', bbox_inches='tight')
+	
+	
+	#plot computation time vs n
+
+	fig = plt.figure(figsize=(16,10))
+
+	z = np.polyfit(n_list, avg_time_compute, 10)
+	p = np.poly1d(z)
+
+	plt.plot(np.linspace(n_list[0], n_list[-1] + 0.1, 100), p(np.linspace(n_list[0], n_list[-1] + 0.1, 100)), ls = '-', color = 'r')
+	plt.plot(n_list, avg_time_compute, ls = '', markersize = 15, marker = '.',label = 'M') #, ls = '--'
+	plt.title('Compilation time scaling for Grovers algorithm, a = 1', fontsize=25)
+	plt.xlabel('n (bit string length)',fontsize=20)
+	plt.ylabel('Average time of computation (ms)',fontsize=20)
+	plt.xticks(fontsize=15)
+	plt.yticks(fontsize=15)
+
+
+	fig.savefig('Figures/grover_compute_a1.png', bbox_inches='tight')
+
+	
+	#plot computation time vs uf for n= 4
+
+	fig = plt.figure(figsize=(16,10))
+
+	
+	plt.plot(input_list, uf_compute_times, ls = '', markersize = 15, marker = '.',label = 'M') #, ls = '--'
+	plt.title('Uf time scaling for Grovers algorithm, n=4, a = 1', fontsize=25)
+	plt.xlabel('n (bit string length)',fontsize=20)
+	plt.ylabel('Average time of computation (ms)',fontsize=20)
+	plt.xticks(fontsize=15)
+	plt.yticks(fontsize=15)
+	fig.savefig('Figures/grover_compute_uf.png', bbox_inches='tight')
---
+++ attiano/grover_readme.txt
@@ -0,0 +1,14 @@
+grover.py contains a set of methods needed to run grovers algorithm in pyquil.
+
+	Create_minus_gate: makes a unitary transformation that maps |x> to -|x>
+	Create_zf: zf = (-1)^f(x)|x>, returns the identity matrix with -1 on the corresponding rows that f(x) returns 1
+	Create_z0: z0 = -|x> if |x> = 0^n else |x>, returns the identity matrix with -1 on first row
+	All_f: creates all possible functions that return f(x)=1 for a given number of qubits n, and number of inputs x that evaluate to 1
+	Calc_lim: calculates how many iterations (k) of grovers algorithm should be ran to ensure it returns the correct value for a given number of qubits, and the number of inputs x that evaluate to 1
+	Grover: runs grovers algorithim k times on the input oracle pyquil gate defintion z_f that corresponds to the oracle function f
+
+To run grover(z0,zf,n,a) you need to input the corresponding gate definition for z0 and zf for n qubits, the number of qubits, and the number of inputs that evaluate to 1. grover() then returns the bit string |x> that corresponds to an input x that evaluates 1.
+	This can be proven by the proof of grovers algorithm that applying G = -(H^n)zo(H^n)zf k times where k is proportional to the sqrt(2^n) sets the state of the qubits to match x with overwhelming probability.
+
+
+If you run it through the command line as the main argument to the python interpreter it runs exhaustive testing and benchmarking for n=1-6 qubits, with the results averaged over 5 trials, and every possible function with a=1 (the number of inputs to f(X) that equal 1 -> |x| s.t. f(x)=1 )
